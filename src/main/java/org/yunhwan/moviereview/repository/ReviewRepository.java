765521bae2b306bce3fe5885b18ffea79e30b53a
yunhwan <pyh8618@gmail.com>
2021-04-20 21:40:09 +0900

    Success. 특정 영화의 모든 리뷰 정보를 조회하는 쿼리파라미터 테스트

--- src/main/java/org/yunhwan/moviereview/repository/ReviewRepository.java
+++ src/main/java/org/yunhwan/moviereview/repository/ReviewRepository.java
@@ -1,9 +1,21 @@
 package org.yunhwan.moviereview.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.yunhwan.moviereview.entity.Movie;
 import org.yunhwan.moviereview.entity.Review;
+
+import java.util.List;
 
 // '회원' 과 '영화'를 댓글로 연결하는 매핑 테이블!
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    /**
+     * 해당 영화에 대한 모든 리뷰 정보를 조회하는 메서드
+     *
+     * +주의할점+ : @EntityGraph를 사용하지 않으면 멤버 객체를 가져올 수 없다 (LAZY 상태 이므로) path에 명시한 것은 Eager로 가져오게 됨.
+     * @param movie
+     * @return 해당 영화의 '리뷰'를 모두 리스트형태로 반환
+     */
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
 }
--- src/test/java/org/yunhwan/moviereview/repository/ReviewRepositoryTest.java
+++ src/test/java/org/yunhwan/moviereview/repository/ReviewRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.yunhwan.moviereview.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.yunhwan.moviereview.entity.Member;
 import org.yunhwan.moviereview.entity.Movie;
 import org.yunhwan.moviereview.entity.Review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,5 +39,30 @@ class ReviewRepositoryTest {
         //when
 
         //then
+    }
+
+    /**
+     * Member Fetch 방식이 LAZY이므로 한번에 두 객체를 조회할 수 없다.
+     *
+     * 따라서, 1. Query를 이용해 조인 처리 2. @EntityGraph를 이용해 Review객체를 가져올 때 Member객체를 로딩시킴
+     * 둘 중 하나의 방법을 사용해야만 한다.
+     * @throws Exception
+     */
+    @Test
+    public void 특정영화의_모든_리뷰_조회() throws Exception {
+        //given
+        Movie movie = Movie.builder().mno(6L).build(); // 댓글 개수 4개 예상
+
+        //when
+        List<Review> result = reviewRepository.findByMovie(movie);
+        //then
+        Assertions.assertThat(4).isEqualTo(result.size());
+        result.forEach(movieReview ->{
+            System.out.print(movieReview.getReviewnum());
+            System.out.print("\t" + movieReview.getGrade());
+            System.out.print("\t" + movieReview.getText());
+            System.out.print("\t" + movieReview.getMember().getEmail());
+            System.out.println("------------END-----------------------");
+        });
     }
 }
