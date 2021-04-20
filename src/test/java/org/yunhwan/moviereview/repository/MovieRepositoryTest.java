11ec36154beb4eab5a16ebf64b9cc8d8b27831ae
yunhwan <pyh8618@gmail.com>
2021-04-20 21:22:40 +0900

    단일 영화의 모든 정보(영화, 모든 이미지, 평균평점, 댓글 갯수)를 가져오는 테스트 성공

--- src/test/java/org/yunhwan/moviereview/repository/MovieRepositoryTest.java
+++ src/test/java/org/yunhwan/moviereview/repository/MovieRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,14 +22,23 @@ class MovieRepositoryTest {
     public void 영화_리뷰_조인을_통해_영화평점평균리뷰갯수_조회() throws Exception {
         //given
         PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "mno")); // size 10 이므로 10개 조회
-
+        //when
         Page<Object[]> result = movieRepository.getListPage(pageRequest);
-
+        //then
         for (Object[] objects : result.getContent()) {
             System.out.println(Arrays.toString(objects));
         }
-        //when
+    }
 
+    @Test
+    public void 영화의_모든_정보를_제대로_가져오는지() throws Exception {
+        //given
+        List<Object[]> result = movieRepository.getMovieWithAll(73L);
+        //when
+        System.out.println(result);
         //then
+        for (Object[] arr : result) {
+            System.out.println(Arrays.toString(arr));
+        }
     }
 }
