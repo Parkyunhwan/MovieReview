60e7d3fe71ab4e1f897aa1b1650aa4a074f6f2e9
yunhwan <pyh8618@gmail.com>
2021-05-03 12:14:36 +0900

    LocalDateTime -> LocalDate로 수정 (년월일만 있으면됨)

--- src/main/java/org/yunhwan/moviereview/dto/MovieDTO.java
+++ src/main/java/org/yunhwan/moviereview/dto/MovieDTO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,8 +38,8 @@ public class MovieDTO {
 
     private LocalDateTime modDate;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime openDate;
+    @DateTimeFormat(pattern = "yyyyMMdd")
+    private LocalDate openDate;
 
     private String rating;
 
--- src/main/java/org/yunhwan/moviereview/entity/Movie.java
+++ src/main/java/org/yunhwan/moviereview/entity/Movie.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -22,7 +23,7 @@ public class Movie extends BaseEntity {
 
     private String title;
 
-    private LocalDateTime openDate;
+    private LocalDate openDate;
 
     private String rating;
 
