6396d6b784960c18b2c22d9f31203f67e3c9650f
yunhwan <pyh8618@gmail.com>
2022-10-17 23:21:53 +0900

    refactor : 영화 등록 rest api로 변경

--- src/main/java/org/yunhwan/moviereview/controller/MovieController.java
+++ src/main/java/org/yunhwan/moviereview/controller/MovieController.java
@@ -2,47 +2,40 @@ package org.yunhwan.moviereview.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpHeaders;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.servlet.view.RedirectView;
 import org.yunhwan.moviereview.dto.MovieDTO;
 import org.yunhwan.moviereview.dto.PageRequestDTO;
 import org.yunhwan.moviereview.dto.PageResultDTO;
 import org.yunhwan.moviereview.service.MovieService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-@Controller
+@RestController
 @RequestMapping("movie")
 @RequiredArgsConstructor
-@Log4j2
+@Slf4j
 public class MovieController {
 
     private final MovieService movieService;
 
-    @GetMapping("register")
-    public void register() {
-
-    }
-
-    @PostMapping("")
-    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
-        log.info("movieDTO: " + movieDTO); // 넘어온 movieDTO
-
-        Long mno = movieService.register(movieDTO);
-
-        redirectAttributes.addFlashAttribute("msg", mno); // 저장후 생성된 mno를 특성에 넣어서 반환
-
-        return "redirect:movie/list"; // 등록 후 영화 목록 페이지로 리다이렉트
+    @PostMapping
+    public Long register(@RequestBody MovieDTO movieDTO) {
+        log.info("movieDTO: " + movieDTO);      // 넘어온 movieDTO
+        return movieService.register(movieDTO);
     }
 
     @GetMapping("list")
