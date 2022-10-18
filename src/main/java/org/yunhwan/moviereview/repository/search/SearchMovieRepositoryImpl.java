3a6b15734a79dfc261771dedbcf7b8da78941c1e
yunhwan <pyh8618@gmail.com>
2022-10-19 01:43:09 +0900

    refactor : queryDsl 빌더 형태로 코드 가독성 개선

--- src/main/java/org/yunhwan/moviereview/repository/search/SearchMovieRepositoryImpl.java
+++ src/main/java/org/yunhwan/moviereview/repository/search/SearchMovieRepositoryImpl.java
@@ -1,19 +1,27 @@
 package org.yunhwan.moviereview.repository.search;
 
+import static org.yunhwan.moviereview.entity.QMovie.*;
+import static org.yunhwan.moviereview.entity.QMovieImage.*;
+import static org.yunhwan.moviereview.entity.QReview.*;
+
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.yunhwan.moviereview.dto.MovieSearchVO;
 import org.yunhwan.moviereview.entity.*;
 
 import java.util.List;
@@ -22,8 +30,11 @@ import java.util.stream.Collectors;
 @Log4j2
 public class SearchMovieRepositoryImpl extends QuerydslRepositorySupport implements SearchMovieRepository {
 
-    public SearchMovieRepositoryImpl() {
+    private final JPAQueryFactory queryFactory;
+
+    public SearchMovieRepositoryImpl(EntityManager em) {
         super(Movie.class);
+        queryFactory = new JPAQueryFactory(em);
     }
 
     @Override
@@ -48,30 +59,47 @@ public class SearchMovieRepositoryImpl extends QuerydslRepositorySupport impleme
     }
 
     @Override
-    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+    public Page<MovieSearchVO> searchPage(String type, String keyword, Pageable pageable) {
         log.info("Movie SearchPage========================+");
+        List<MovieSearchVO> result = queryFactory
+                .select(Projections.fields(MovieSearchVO.class,
+                        movie, movieImage, review.grade.avg().coalesce(0.0).as("avg"), review.count().as("reviewCnt"))
+                )
+                .from(movie)
+                .leftJoin(movieImage)
+                .on(movie.eq(movieImage.movie))
+                .leftJoin(review)
+                .on(movie.eq(review.movie))
+                .where(mnoGreatherThanZero()
+                        .and(isSearchable(type, keyword))
+                )
+                .groupBy(movie)
+                .orderBy(getOrderSpecifier(pageable.getSort()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        int count = result.size();
 
-        QMovie movie = QMovie.movie;
-        QReview review = QReview.review;
-        QMovieImage movieImage = QMovieImage.movieImage;
+        return new PageImpl<>(
+                result
+                , pageable
+                , count
+        );
+    }
 
-        JPQLQuery<Movie> jpqlQuery = from(movie);
-        jpqlQuery.leftJoin(movieImage).on(movieImage.movie.eq(movie));
-        jpqlQuery.leftJoin(review).on(review.movie.eq(movie));
+    private OrderSpecifier[] getOrderSpecifier(Sort sort) {
+        return sort.stream().map(order -> {
+            Order direction = order.isAscending() ? Order.ASC : Order.DESC;
+            String prop = order.getProperty();
+            PathBuilder orderByExpression = new PathBuilder(Movie.class, "movie");
+            return new OrderSpecifier(direction, orderByExpression.get(prop));
+        }).toArray(OrderSpecifier[]::new);
+    }
 
-
-        JPQLQuery<Tuple> tuple = jpqlQuery.select(movie, movieImage, review.grade.avg().coalesce(0.0), review.count());
-
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
-        BooleanExpression expression = movie.mno.gt(0L);
-
-        booleanBuilder.and(expression);
-
+    private static BooleanBuilder isSearchable(String type, String keyword) {
+        BooleanBuilder conditonBuilder = new BooleanBuilder();
         if (type != null) {
             String[] typeArr = type.split("");
-
-            BooleanBuilder conditonBuilder = new BooleanBuilder();
-
             for (String t : typeArr) {
                 switch (t) {
                     case "t":
@@ -85,35 +113,12 @@ public class SearchMovieRepositoryImpl extends QuerydslRepositorySupport impleme
                         break;
                 }
             }
-            booleanBuilder.and(conditonBuilder);
+            return conditonBuilder;
         }
-        tuple.where(booleanBuilder);
+        return conditonBuilder;
+    }
 
-        //order by
-        Sort sort = pageable.getSort();
-
-        sort.stream().forEach(order -> {
-            Order direction = order.isAscending()?Order.ASC: Order.DESC;
-            String prop = order.getProperty();
-
-            PathBuilder orderByExpression = new PathBuilder(Movie.class, "movie");
-            tuple.orderBy(new OrderSpecifier(direction, orderByExpression.get(prop)));
-        });
-        //
-        tuple.groupBy(movie);
-
-        // page처리를 위한 부분
-        tuple.offset(pageable.getOffset());
-        tuple.limit(pageable.getPageSize());
-
-        List<Tuple> result = tuple.fetch();
-
-        log.info(result);
-        long count = tuple.fetchCount();
-        log.info("fetch count : " + count);
-
-        return new PageImpl<Object[]>(
-                result.stream().map(t -> t.toArray()).collect(Collectors.toList())
-                , pageable, count);
+    private static BooleanExpression mnoGreatherThanZero() {
+        return movie.mno.gt(0L);
     }
 }
