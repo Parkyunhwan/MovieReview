08ba9a10cc3534a5b5690166f7978c5b24f1c046
yunhwan <pyh8618@gmail.com>
2022-10-21 21:33:51 +0900

    refactor : ResponseEntity추가

--- src/main/java/org/yunhwan/moviereview/controller/MovieController.java
+++ src/main/java/org/yunhwan/moviereview/controller/MovieController.java
@@ -3,7 +3,6 @@ package org.yunhwan.moviereview.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -32,31 +31,22 @@ public class MovieController {
     private final MovieService movieService;
 
     @PostMapping
-    public Long register(@RequestBody MovieDTO movieDTO) {
+    public ResponseEntity<Long> register(@RequestBody MovieDTO movieDTO) {
         log.info("movieDTO: " + movieDTO);      // 넘어온 movieDTO
-        return movieService.register(movieDTO);
+        return ResponseEntity.ok(movieService.register(movieDTO));
     }
 
     @GetMapping
-    public Page<MovieSearchResponseDTO> list(MovieSearchRequestDTO movieSearchRequestDTO, Pageable pageable) {
-        return movieService.getList(movieSearchRequestDTO, pageable);
+    public ResponseEntity<Page<MovieSearchResponseDTO>> list(MovieSearchRequestDTO movieSearchRequestDTO,
+            Pageable pageable) {
+        return ResponseEntity.ok(movieService.getList(movieSearchRequestDTO, pageable));
     }
 
     @GetMapping("{mno}")
-    public MovieDTO readMovie(@PathVariable long mno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO) {
+    public ResponseEntity<MovieDTO> readMovie(@PathVariable long mno,
+            @ModelAttribute("requestDTO") PageRequestDTO requestDTO) {
         log.info("mno: " + mno);
-        return movieService.getMovie(mno);
-    }
-
-    @GetMapping("{mno}/modify")
-    public String modifyPage(@PathVariable long mno, @ModelAttribute("requestDTO") PageRequestDTO requestDTO, Model model) {
-        log.info("mno: " + mno);
-
-        MovieDTO movieDTO = movieService.getMovie(mno);
-
-        model.addAttribute("dto", movieDTO);
-
-        return "movie/modify";
+        return ResponseEntity.ok(movieService.getMovie(mno));
     }
 
     @PutMapping("/{mno}")
