278864c9755ec6f2a8c334bfc3db375c019371c4
yunhwan <pyh8618@gmail.com>
2022-10-28 21:53:19 +0900

    refactor : import 최적화

--- src/main/java/org/yunhwan/moviereview/controller/UploadController.java
+++ src/main/java/org/yunhwan/moviereview/controller/UploadController.java
@@ -1,16 +1,12 @@
 package org.yunhwan.moviereview.controller;
 
-import java.nio.file.FileSystems;
 import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnailator;
-import org.apache.tomcat.jni.Local;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.util.FileSystemUtils;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
