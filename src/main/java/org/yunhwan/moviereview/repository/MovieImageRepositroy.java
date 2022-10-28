05e445239e8f4724f15326d359f8c65a2b5b36d2
yunhwan <pyh8618@gmail.com>
2022-10-28 21:04:30 +0900

    refactor : 영화에 대한 컬렉션인 영화이미지를 가져오기 위한 메서드 추가

--- src/main/java/org/yunhwan/moviereview/repository/MovieImageRepositroy.java
+++ src/main/java/org/yunhwan/moviereview/repository/MovieImageRepositroy.java
@@ -3,12 +3,21 @@ package org.yunhwan.moviereview.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.yunhwan.moviereview.dto.MovieImageDTO;
 import org.yunhwan.moviereview.entity.Movie;
 import org.yunhwan.moviereview.entity.MovieImage;
+
+import java.util.List;
 
 public interface MovieImageRepositroy extends JpaRepository<MovieImage, Long> {
 
     @Modifying
     @Query("delete from MovieImage mi where mi.movie=:movie")
     void deleteByMovie(Movie movie);
+
+    @Query("select mi from MovieImage mi " +
+            "where mi.movie.id = :movieId")
+    List<MovieImageDTO> findAllMovieImageByMovieId(@Param("movieId") Long movieId);
+
 }
