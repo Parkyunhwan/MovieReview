eea103bc1b26200c632d7f68122273c44eb0eaf7
yunhwan <pyh8618@gmail.com>
2022-10-28 21:03:52 +0900

    refactor : 컬렉션을 제외한 영화조회정보를 가져오도록 Movie Query용 Repository 추가
    - 복잡한 MovieRepository.java Movie조회 쿼리 삭제

--- src/main/java/org/yunhwan/moviereview/repository/MovieRepository.java
+++ src/main/java/org/yunhwan/moviereview/repository/MovieRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.yunhwan.moviereview.dto.MovieResponseDTO;
 import org.yunhwan.moviereview.entity.Movie;
 import org.yunhwan.moviereview.repository.search.SearchMovieRepository;
 
@@ -16,20 +17,4 @@ public interface MovieRepository extends JpaRepository<Movie, Long>, SearchMovie
             + "left outer join MovieImage mi on mi.movie = m "
             + "left outer join Review r on r.movie = m group by m ")
     Page<Object[]> getListPage(Pageable pageable);
-
-
-    /**
-     * 영화 조회 시 영화와 해당 영화의 모든 이미지, 평균 평점 + 리뷰 개수를 함께 조회해오기 위한 기능
-     *
-     * group by를 통해 '영화 이미지'별로 묶고
-     * where를 통해 특정 영화만을 조회한다.
-     *
-     */
-    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(r) from Movie m "
-            + "left outer join MovieImage mi on mi.movie = m "
-            + "left outer join Review r on r.movie = m "
-            + "where m.id = :mno group by mi")
-    List<Object[]> getMovieWithAll(Long mno);
-
-
 }
